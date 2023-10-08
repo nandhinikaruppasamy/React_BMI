2d6b210b554a124a82f102087ff5a052c4f94dca
Nandhini <nandhinikaruppasamy1402@gmail.com>
2023-10-08 23:00:17 +0530

    First Commit

---
+++ cal/App.js
@@ -0,0 +1,17 @@
+import { StyleSheet, View } from 'react-native';
+import BmiCalculator from './src';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <BmiCalculator/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    
+  },
+});
